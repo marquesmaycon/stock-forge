4d3f9037009d686b8b01c55720b7c176df26cc6f
marquesmaycon <mayconmarquesh@gmail.com>
2026-03-01 16:27:13 -0300

    tuyau react query

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,7 +1,10 @@
 import { createTuyau } from '@tuyau/core/client'
+import { createTuyauReactQueryClient } from '@tuyau/react-query'
 import { registry } from '@api-starter-kit/backend/registry'
 
-export const api = createTuyau({
+export const client = createTuyau({
   baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:3333',
   registry,
 })
+
+export const api = createTuyauReactQueryClient({ client })
